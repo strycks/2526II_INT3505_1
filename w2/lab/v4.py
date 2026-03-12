84dd9f06236d6736e870fa114a79414123c9df1b
Nguyễn Thế Ngọc Hà <88977475+Strkss@users.noreply.github.com>
2026-03-12 10:48:56 +0700

    added curl commands for testing

--- w2/lab/v4.py
+++ w2/lab/v4.py
@@ -10,10 +10,12 @@ def hello_world():
 
 @app.route('/users', methods=['GET', 'POST'])
 def handle_users():
+    # curl "http://127.0.0.1:5000/users"
     if (request.method == 'GET'):
         response = make_response(jsonify({"users": users}))
         response.headers['Cache-Control'] = 'public, max-age=60'
         return response
+    # curl -X POST "http://127.0.0.1:5000/users" -d '{"id": "2", "name": "invoker"}' -H "Content-Type: application/json"
     elif (request.method == 'POST'):
         new_user = request.get_json()
         users.append(new_user)
@@ -23,10 +25,12 @@ API_KEY = "super_secret"
 
 @app.route('/secret', methods=['GET'])
 def handle_secrets():
+    # curl "http://127.0.0.1:5000/secret" -H "Authorization: super_secret"
     client_key = request.headers.get('Authorization')
     
     if client_key == API_KEY:
         return 'secret information', 200
     else:
         return 'unauthorized', 401
+
 app.run(debug=True)
