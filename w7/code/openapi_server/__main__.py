68c98412997b24421fd9bb6839a00d1193663cab
Nguyễn Thế Ngọc Hà <88977475+Strkss@users.noreply.github.com>
2026-04-16 16:42:31 +0700

    feat: register global error handler

--- w7/code/openapi_server/__main__.py
+++ w7/code/openapi_server/__main__.py
@@ -6,9 +6,15 @@ from mongoengine import connect
 from flask_jwt_extended import JWTManager
 import os
 from database.utils import create_admin
+from mongoengine import ValidationError, DoesNotExist
 
 from openapi_server import encoder
 
+def handle_validation_error(e):
+    return {"title": "Resource Validation Error", "details": str(e), "status": 400}, 400
+
+def handle_not_found(e):
+    return {"title": "Resource not found", "details": str(e), "status": 404}, 404
 
 def main():
     load_dotenv()
@@ -16,6 +22,10 @@ def main():
     
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder  
+    
+    app.app.register_error_handler(ValidationError, handle_validation_error)
+    app.app.register_error_handler(DoesNotExist, handle_not_found)
+    
     app.add_api('openapi.yaml',
                 arguments={'title': 'Simplified Library Management API'},
                 pythonic_params=True)
--- w7/code/services/book_service.py
+++ w7/code/services/book_service.py
@@ -4,7 +4,7 @@ def get_all_books():
     return list(Book.objects.all())
 
 def get_book_by_id(b_id):
-    return Book.objects(id=b_id).first()
+    return Book.objects.get(id=b_id)
 
 def create_new_book(data):
     data = data.to_dict()
@@ -12,7 +12,7 @@ def create_new_book(data):
     return new_book.save()
 
 def update_existing_book(b_id, data):
-    book = Book.objects(id=b_id).first()
+    book = Book.objects.get(id=b_id)
     data = data.to_dict()
     if book:
         book.update(**data)
